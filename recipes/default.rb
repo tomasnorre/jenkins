f98762e11c2b707bb749086f99bdb5b036703d89
Tomas Mikkelsen <tomas.mikkelsen@aoe.com>
2014-07-31 21:21:45 +0200

    Changing code with respect to rubocop

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-jenkins_cli = "/run/jenkins/war/WEB-INF/jenkins-cli.jar"
+jenkins_cli = '/run/jenkins/war/WEB-INF/jenkins-cli.jar'
 if defined? node['jenkins']['cli'] && node['jenkins']['cli']
   jenkins_cli = node['jenkins']['cli']
 end
@@ -25,10 +25,16 @@ service 'jenkins' do
   action [:enable, :start]
 end
 
+#
+group 'www-data' do
+  action :modify
+  members 'maintenance'
+  append true
+end
 
-bash "install_jenkins_plugin" do
-  user "root"
-  cwd "/tmp"
+bash 'install_jenkins_plugin' do
+  user 'root'
+  cwd '/tmp'
   code <<-EOH
   java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin greenballs credentials ssh-credentials git git-client token-macro scm-api gravatar
   java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin template-project run-condition flexible-publish envfile envinject
