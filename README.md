c32e2af82d6a1354e8181aafea1eed2e7824bf7b
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2015-03-06 22:14:46 +0100

    Added private key for installing plugins

--- README.md
+++ README.md
@@ -17,6 +17,14 @@ Just include `jenkins` in your node's `run_list`:
 }
 ```
 
+This cookbook creates a system user "jenkins", it's recommended to create a ssh key pair for this user, and map it to a jenkins user, that can install plugins etc.
+The second you active login on jenkins, you will not be able to install plugins through this cookbook anymore without this set.
+
+```text
+sudo su jenkins
+ssh-keygen -t rsa -C "jenkins@domain.tld"
+```
+
 License and Authors
 -------------------
 Authors:
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,7 @@
 #
 
 jenkins_cli = '/run/jenkins/war/WEB-INF/jenkins-cli.jar'
+private_key = '/var/lib/jenkins/.ssh/id_rsa'
 if defined? node['jenkins']['cli'] && node['jenkins']['cli']
   jenkins_cli = node['jenkins']['cli']
 end
@@ -48,11 +49,11 @@ bash 'install_jenkins_plugin' do
   cwd '/tmp'
   code <<-EOH
   curl  -L http://updates.jenkins-ci.org/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://localhost:8080/updateCenter/byId/default/postBack
-  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin greenballs git git-client token-macro
-  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin credentials ssh-credentials scm-api gravatar
-  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin template-project run-condition
-  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin flexible-publish envfile envinject ws-cleanup
-  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ install-plugin config-autorefresh-plugin build-pipeline-plugin
+  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin greenballs git git-client token-macro
+  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin credentials ssh-credentials scm-api gravatar
+  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin template-project run-condition
+  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin flexible-publish envfile envinject ws-cleanup
+  java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin config-autorefresh-plugin build-pipeline-plugin
   EOH
   notifies :restart, 'service[jenkins]'
 end
