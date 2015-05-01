d3fec7d417f47c134b519ac93de6cc6715d6e292
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2015-05-01 20:29:06 +0200

    Update

--- recipes/default.rb
+++ recipes/default.rb
@@ -90,7 +90,7 @@ bash 'install_jenkins_plugin' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  curl  -L http://updates.jenkins-ci.org/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://localhost:8080/updateCenter/byId/default/postBack
+  curl -L http://updates.jenkins-ci.org/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://localhost:8080/updateCenter/byId/default/postBack
   for p in `cat jenkins-plugins-to-install` ; do
     java -jar #{jenkins_cli} -s http://127.0.0.1:8080/ -i #{private_key} install-plugin $p ;
   done
