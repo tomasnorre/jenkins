b64e2fe18af6fa8a467a0be42d736483c9e4f65c
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2016-07-14 21:39:34 +0200

    [TASK] Update for Jenkins 2.x

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'All rights reserved'
 description 'Installs/Configures jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.0'
+issues_url 'https://github.com/tomasnorre/jenkins/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/tomasnorre/jenkins' if respond_to?(:source_url)
 
 depends 'java'
 
